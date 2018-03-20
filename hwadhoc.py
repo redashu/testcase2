8883bb06b869ef6f287f429bfedcbde511feb60b
redashu <ashutoshh@linux.com>
2018-03-20 07:17:52 +0000

    commiitttt

--- hwadhoc.py
+++ hwadhoc.py
@@ -2,7 +2,7 @@
 
 def  hello():
     print("test case 2 start okkkkk")
-    print("after 2nd commit i wiil execute myself")
+    print("after 2nd for timer commit i wiil execute myself")
     for  i in range(10):
         print(10*i)
 
