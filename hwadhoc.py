0318c4a4c896fdca54a2df5bb92b2b91cbe5bf31
redashu <ashutoshh@linux.com>
2018-03-20 07:25:13 +0000

    done with done

--- hwadhoc.py
+++ hwadhoc.py
@@ -3,6 +3,7 @@
 def  hello():
     print("test case 2 start okkkkk")
     print("after 2nd for timer commit i wiil execute myself")
+    print("Going  forward with cloud")
     for  i in range(10):
         print(10*i)
 
